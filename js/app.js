a79f20a64a0fc498b7e4bda37ba861ce0458908c
sugarcookie <latasha.hussey@gmail.com>
2017-04-06 14:54:36 -0500

    Add more cats

--- js/app.js
+++ js/app.js
@@ -1,9 +1,63 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/');
-    this.nicknames = ko.observableArray(['Shaft','Cutie Pie','Sam I Am']);
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ['Shaft','Cutie Pie','Sam I Am']
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicknames: ['Tigger']
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+        nicknames: ['Fraidy']
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Grey']
+    },
+    {
+        clickCount : 0,
+        name : 'SweetPea',
+        imgSrc : 'img/sweetpea.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Bean']
+    },
+    {
+        clickCount : 0,
+        name : 'Toby',
+        imgSrc : 'img/toby.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Obie One']
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames: ['Shuteye']
+    }
+];
+
+
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     // Cat Levels
     this.catLevel = ko.computed(function() {
@@ -29,12 +83,22 @@ var Cat = function() {
 //Setup View Model for those things we need to change
 var ViewModel = function() {
 
+    var self = this;
+
+    // store cats
+    this.catList = ko.observableArray([]);
+
+    //loop over cats
+    initialCats.forEach(function(catItem) {
+        self.catList.push( new Cat(catItem));
+    });
+
     // Store current cat in variable
-    this.currentCat = ko.observable(new Cat());
+    this.currentCat = ko.observable(this.catList()[0]);
 
     //Increment the counter
-    this.incrementCounter = function() {
-      this.clickCount(this.clickCount() + 1);
+    self.incrementCounter = function() {
+      self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 };
 
